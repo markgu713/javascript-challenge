de1f78ca1ed8c4300fee323067e9ba9c7a4327b8
Mark Gu <67448433+markgu713@users.noreply.github.com>
2020-11-19 23:15:37 -0500

    display all ufo sighting data in tables

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputDate = d3.select("#datetime");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+// function to append all data in table rows
+function displayAllData(dataSet) {
+    dataSet.forEach(dataObject => {
+        var row =tbody.append("tr");
+        columns.forEach(column => row.append("td").text(dataObject[column]));
+    });
+};
+
+//Populate data in table
+displayAllData(data);
+
+
+
+//var inputCity= d3.select("#city");
+//var resetbtn = d3.select("#reset-btn");
