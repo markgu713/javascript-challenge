a2c293dd744c237ffa40a1bd90c4ace75e9728a1
Mark Gu <67448433+markgu713@users.noreply.github.com>
2020-11-21 12:23:52 -0500

    level2 dropdown selection search

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -5,6 +5,10 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputDate = d3.select("#datetime");
+var inputCity = d3.select("#city");
+var inputState = d3.select("#state");
+var inputCountry = d3.select("#country");
+var inputShape = d3.select("#shape");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 // function to append all data in table rows
@@ -12,6 +16,23 @@ function displayData(dataSet) {
     dataSet.forEach(dataObject => {
         var row =tbody.append("tr");
         columns.forEach(column => row.append("td").text(dataObject[column]));
+    });
+};
+
+// populate options under select
+function addDropdownOption(field) {
+    var selectTag = document.getElementById(field);
+    var optionArray = [];
+    tableData.forEach(dataObject => {
+        if (optionArray.indexOf(dataObject[field])> -1) {
+            // do nothing
+        } else {
+            optionArray.push(dataObject[field]);
+            let optEle = document.createElement("option");
+            optEle.text = dataObject[field];
+            optEle.value = dataObject[field];
+            selectTag.add(optEle);
+        };
     });
 };
 
@@ -23,9 +44,13 @@ button.on("click", search)
 // Filter by attribute
 function search() {
 	d3.event.preventDefault();
-	var date = inputDate.property("value").trim();
+    var date = inputDate.property("value");
+    var city = inputCity.property("value");
+    var state = inputState.property("value");
+    var country = inputCountry.property("value");
+    var shape = inputShape.property("value");
 	// Filter by field matching input value
-	var filterData = data.filter(data => data.datetime === date);
+    var filterData = tableData.filter(data => data.datetime === date && data.city === city && data.state === state && data.country === country && data.shape === shape);
 	// clean the search results
     tbody.html("");
     // return search results
